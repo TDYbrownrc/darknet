4af3143adfc2fbf9e70a555c5a4a4ba6de923190
Ryan Brown <ryan.brown@teledyne.com>
2020-07-08 21:08:42 +0000

    Updated to fix some of the required functionality

--- src/network_kernels.cu
+++ src/network_kernels.cu
@@ -674,7 +674,7 @@ float *get_network_output_gpu(network net)
     for(i = net.n-1; i > 0; --i) if(net.layers[i].type != COST) break;
 
     //highest resolution conv layer before yolo head
-    get_network_output_gpu_layer(net, 138);
+    get_network_output_layer_gpu(net, 138);
 
     return get_network_output_layer_gpu(net, i);
 }
